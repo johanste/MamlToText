670a25dfdc135cbe41f7b41bbf4dac3f7b1c21ba
Johan Stenberg <johan.stenberg@microsoft.com>
2016-01-08 16:28:07 -0800

    Move files to src directory

---
+++ src/MamlToText/InstalledModuleInfo.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CLU.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Microsoft.CLU
+{
+    internal class InstalledModuleInfo
+    {
+        public LocalPackage Package { get; set; }
+        public IList<InstalledCmdletInfo> Cmdlets { get; set; }
+
+        public static IEnumerable<InstalledModuleInfo> Enumerate(CmdletLocalPackage package, IEnumerable<string> commandDiscriminators)
+        {
+            var installedModuleInfos = new List<InstalledModuleInfo>();
+
+            var matchedCmdlets = package.FindMatchingCommandlets(commandDiscriminators, false);
+            if (matchedCmdlets.Count() > 0)
+            {
+                var module = new InstalledModuleInfo { Package = package, Cmdlets = new List<InstalledCmdletInfo>() };
+                foreach (var entry in matchedCmdlets)
+                {
+       
+                    module.Cmdlets.Add(new InstalledCmdletInfo
+                    {
+                        Keys = String.Join(";", entry.CommandDiscriminators),
+                        AssemblyName = entry.Package.DefaultAssembly.GetName().Name,
+                        Type = entry.LoadCmdlet()
+                    });
+                }
+
+                installedModuleInfos.Add(module);
+            }
+
+            return installedModuleInfos;
+        }
+    }
+
+    internal class InstalledCmdletInfo
+    {
+        public string Keys { get; set; }
+        public string CommandName { get; set; }
+        public string AssemblyName { get; set; }
+        public Type Type { get; set; }
+        public Help.MAMLReader.CommandHelpInfo Info { get; set; }
+    }
+}
---
+++ src/MamlToText/ParameterMetadata.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace System.Management.Automation
+{
+    public sealed class ParameterMetadata
+    {
+        public ParameterMetadata(ParameterMetadata other)
+        {
+            this.Name = other.Name;
+            this.ParameterSets = other.ParameterSets;
+            this._mandatory = other._mandatory;
+            this._position = other._position;
+        }
+
+        public ParameterMetadata(string name)
+        {
+            this.Name = name;
+            this.ParameterSets = new Dictionary<string, ParameterSetMetadata>();
+        }
+
+        public ParameterMetadata(string name, Type parameterType) : this(name)
+        {
+            this.ParameterType = parameterType;
+        }
+
+        public ParameterMetadata(PropertyInfo info) : this(info.Name, info.PropertyType)
+        {
+            Debug.Assert(info != null);
+            this._property = info;
+            var attributes = info.GetCustomAttributes();
+
+            foreach (var attr in _property.GetCustomAttributes())
+            {
+                _attributes.Add(attr);
+                var pAttr = attr as ParameterAttribute;
+                if (pAttr != null)
+                {
+                    SetMandatory(pAttr.ParameterSetName, pAttr.Mandatory);
+                    SetPosition(pAttr.ParameterSetName, pAttr.Position);
+                }
+            }
+        }
+
+        public IEnumerable<string> Aliases
+        {
+            get
+            {
+                var attributes = _property != null ? _property.GetCustomAttributes() : null;
+                return attributes != null ?
+                    attributes.Where(a => a is AliasAttribute).Select(a => a as AliasAttribute).SelectMany(a => a.AliasNames) :
+                    new string[0];
+            }
+        }
+
+        public Collection<Attribute> Attributes { get { return _attributes; } }
+
+        public bool IsDynamic { get; set; }
+
+        internal int Position(string parameterSet)
+        {
+            if (!string.IsNullOrEmpty(parameterSet))
+            {
+                if (_position.ContainsKey(parameterSet))
+                    return _position[parameterSet];
+            }
+            if (_position.ContainsKey("AllParameterSets"))
+                return _position["AllParameterSets"];
+            return -1;
+        }
+
+        internal void SetPosition(string parameterSet, int position)
+        {
+            if (position != -1)
+            {
+                if (string.IsNullOrEmpty(parameterSet))
+                    parameterSet = "AllParameterSets";
+                _position[parameterSet] = position;
+            }
+        }
+
+        internal bool IsMandatory(string parameterSet)
+        {
+            if (!string.IsNullOrEmpty(parameterSet))
+            {
+                if (_position.ContainsKey(parameterSet))
+                    return _mandatory[parameterSet];
+            }
+            if (_mandatory.ContainsKey("AllParameterSets"))
+                return _mandatory["AllParameterSets"];
+            return false;
+        }
+
+        internal void SetMandatory(string parameterSet, bool isMandatory)
+        {
+            if (string.IsNullOrEmpty(parameterSet))
+                parameterSet = "AllParameterSets";
+            _mandatory[parameterSet] = isMandatory;
+        }
+
+        public string Name { get; set; }
+
+        public Dictionary<string, ParameterSetMetadata> ParameterSets { get; private set; }
+
+        public Type ParameterType { get; set; }
+
+        public bool SwitchParameter { get { return this.ParameterType.Equals(typeof(SwitchParameter)); } }
+
+        internal bool IsBound { get; set; }
+
+        internal bool IsBuiltin { get; set; }
+
+
+        private void Validate(object argument)
+        {
+        }
+
+        private PropertyInfo _property;
+        private Collection<Attribute> _attributes = new Collection<Attribute>();
+        private Dictionary<string, int> _position = new Dictionary<string, int>();
+        private Dictionary<string, bool> _mandatory = new Dictionary<string, bool>();
+    }
+}
